229477f94a8be5c34bb78773ef2a901b4f388e42
akhilkodakara <me.akhil05@gmail.com>
2024-10-05 12:29:18 +0530

    modify company's edit profile popup

--- web-app/src/app/features/company-menu/company-profile/edit-profile/edit-profile.component.ts
+++ web-app/src/app/features/company-menu/company-profile/edit-profile/edit-profile.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from "@angular/common";
-import { Component, Inject } from "@angular/core";
+import { Component, Inject, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, FormsModule } from "@angular/forms";
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
 import { AMGModules } from "src/AMG-Module/AMG-module";
@@ -18,7 +18,7 @@ import {
   styleUrl: "./edit-profile.component.css",
 })
 export class EditProfileComponent {
-  // formData: FormGroup;
+  formData: FormGroup;
   sectorChips: string[] = [
     "Internet",
     "Saas",
@@ -30,11 +30,31 @@ export class EditProfileComponent {
   constructor(
     private fb: FormBuilder,
     public dialogRef: MatDialogRef<EditProfileComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any // Data passed from the company-profile.component
-  ) {}
+    // @Inject(MAT_DIALOG_DATA) public data: any // Data passed from the company-profile.component
+  ) {
+    this.formData = this.fb.group({
+      sector: [''],
+      overview: [''],
+    });
+  }
   ngOnInit(): void {
-    this.initializeForm();
     this.loadInitialData();
+    this.initializeForm();
+  }
+  initializeForm(): void {}
+
+  removeSectorChips(sector: string): void {
+    console.log("hiii");
+    const index = this.sectorChips.indexOf(sector);
+    if (index >= 0) {
+      this.sectorChips.splice(index, 1);
+      console.log(`removed ${sector} from sector chips`);
+      // return [...this.sectorChips];
+    }
+    // return this.sectorChips;
+  }
+
+  loadInitialData(): void {
     this.sectorChips = [
       "Internet",
       "Saas",
@@ -43,9 +63,6 @@ export class EditProfileComponent {
       "Private",
       "Startup",
     ];
-  }
-  initializeForm(): void {}
-  loadInitialData(): void {
     // Load the data passed in the dialog from the company profile component.
   }
 }
