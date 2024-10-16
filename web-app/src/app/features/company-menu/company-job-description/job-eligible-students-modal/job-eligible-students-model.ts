573c81a4c82ef703d8780569a3faa658b7b6bf8a
Naveenv8610 <naveenv.xworkz@gmail.com>
2024-10-16 15:21:43 +0530

    Added extra fields for  apllied students table in compnay module

--- web-app/src/app/features/company-menu/company-job-description/job-eligible-students-modal/job-eligible-students-model.ts
+++ web-app/src/app/features/company-menu/company-job-description/job-eligible-students-modal/job-eligible-students-model.ts
@@ -1,8 +1,12 @@
 export type jobEligibleStudent = {
   StudentID: number;
+  DegreeName: string;
+  CollegeName: string;
   StudentName: string;
   Branch: string;
+  JobeRole: string;
   Batch: string;
   CGPA: string;
   Status: string;
+  resume: string | null;
 };
