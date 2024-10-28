1c6c039243d5c01ac654bc0b8ae4c91b5d56ce99
vaishnavishesh <vaishanvisacharya14@gmail.com>
2024-10-28 10:01:11 +0530

    interview get integration

--- web-app/src/app/features/placement-cell/placement-cell/placement-interview/api.interview.ts
+++ web-app/src/app/features/placement-cell/placement-cell/placement-interview/api.interview.ts
@@ -3,6 +3,7 @@ import { Injectable } from "@angular/core";
 import { catchError, firstValueFrom, Observable, throwError } from "rxjs";
 import { ApiHttpService } from "src/app/services/api-services/api-http-services";
 import { Jobinterviewround } from "src/app/services/types/Jobinterviewround";
+import { ODataEntity } from "src/app/services/types/OData";
 
 @Injectable({
   providedIn: "root",
@@ -10,43 +11,11 @@ import { Jobinterviewround } from "src/app/services/types/Jobinterviewround";
 export class interviewApiService {
   constructor(private apiHttpService: ApiHttpService) {}
 
-  // public async GetAllRounds(): Promise<any> {
-  //   try {
-  //     const response = await firstValueFrom(
-  //       this.apiService
-  //         .get("/Jobinterviewround")
-  //         .pipe(catchError(this.handleError))
-  //     );
-  //     return response;
-  //   } catch (error) {
-  //     if (error instanceof Error) {
-  //       throw new Error(`Error in GET request: ${error.message}`);
-  //     } else {
-  //       throw error;
-  //     }
-  //   }
-  //   // return this.apiService.get<Jobinterviewround[]>("/Jobinterviewround");
-  // }
-  Getinterview(): Observable<any> {
-    return this.apiHttpService.get<any>("/Jobinterviewround");
+  GetJobInterviewRounds(): Observable<ODataEntity<Jobinterviewround[]>> {
+    return this.apiHttpService.get<ODataEntity<Jobinterviewround[]>>(
+      "/Jobinterviewround?$expand=JobPosting($select=JobRole,JobDescription,ValidFrom,ValidTill;$expand=Company($select=Name,LogoPath)),JobpostStudentrounds"
+    );
   }
 
-  //   GetRoundsById(id: number): Observable<any> {
-  //     return this.apiHttpService.get(`/Jobinterviewround?filter=Id eq ${id}`);
-  //   }
 
-  //   deleteRound(id: number): Observable<any> {
-  //     const url = `/Trainingcourse?key=${id}`;
-  //     const data = { isdeleted: true };
-  //     return this.apiHttpService.patch(url, data);
-  //   }
-
-  //   addUpdateRounds(
-  //     id: number | null,
-  //     TrainingcourseData: Partial<any>
-  //   ): Observable<any> {
-  //     const url = `/Trainingcourse?key=${id ? id : ""}`;
-  //     const method = id ? "patch" : "post";
-  //     return this.apiHttpService[method](url, TrainingcourseData);
-  //   }
 }
