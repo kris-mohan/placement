2a7a3b733094d9323c91f001ca17f00c9d6885d2
Mahadev R Saravad <mahadevrsaravad@gmail.com>
2024-11-09 16:53:08 +0530

    upload doc in company profile

--- web-app/src/app/features/company-menu/company-profile/edit-profile/edit-profile.component.ts
+++ web-app/src/app/features/company-menu/company-profile/edit-profile/edit-profile.component.ts
@@ -18,11 +18,25 @@ import {
 import { SweetAlertService } from "src/app/services/sweet-alert-service/sweet-alert-service";
 import { EditProfileApiService } from "./api.edit-profile";
 import { Industry } from "src/app/services/types/Industry";
+import { DateTime } from "luxon";
 // import { LiveAnnouncer } from "@angular/cdk/a11y";
 
 type Sector = {
   Name?: string;
 };
+
+interface UploadedFileInfo {
+  fileName: string;
+  filePath: string;
+  fileType: string;
+  timestampedFileName: string;
+  errorMessage: string | null;
+  parentType: string;
+  parentId: number;
+  isDeleted: boolean;
+  createdDate: Date;
+  createdBy: string;
+}
 
 @Component({
   selector: "app-edit-profile",
@@ -43,6 +57,11 @@ export class EditProfileComponent implements OnInit {
   readonly sectorChips = signal<Sector[]>([]);
 
   industries: Industry[] = [];
+  uploadedFiles: UploadedFileInfo[] = [];
+  selectedVideos: File[] = [];
+  selectedPpts: File[] = [];
+  selectedAudios: File[] = [];
+  selectedDocuments: File[] = [];
 
   showAllSectors = false;
 
@@ -62,6 +81,10 @@ export class EditProfileComponent implements OnInit {
       companySize: "",
       headquarters: "",
       website: "",
+      audio: [null],
+      video: [null],
+      ppt: [null],
+      document: [null],
     });
   }
   ngOnInit(): void {
@@ -172,6 +195,92 @@ export class EditProfileComponent implements OnInit {
     },
   ];
 
+  onVideoSelected(event: any): void {
+    this.selectedVideos = Array.from(event.target.files);
+    this.addEditCompanyProfile.patchValue({ video: this.selectedVideos });
+  }
+  onPptSelected(event: any): void {
+    this.selectedPpts = Array.from(event.target.files);
+    this.addEditCompanyProfile.patchValue({ ppt: this.selectedPpts });
+  }
+  onAudioSelected(event: any): void {
+    this.selectedAudios = Array.from(event.target.files);
+    this.addEditCompanyProfile.patchValue({ audio: this.selectedAudios });
+  }
+  onDocumentSelected(event: any): void {
+    this.selectedDocuments = Array.from(event.target.files);
+    this.addEditCompanyProfile.patchValue({ document: this.selectedDocuments });
+  }
+
+  uploadDocument(selectedFiles: File[]): void {
+    if (selectedFiles.length > 0) {
+      const formData = new FormData();
+      selectedFiles.forEach((file) => formData.append("files", file));
+
+      this.editProfileApiService.uploadDocument(formData).subscribe({
+        next: (response) => {
+          if (response.success && response.files) {
+            response.files.flatMap((f: any) => {
+              this.uploadedFiles = [
+                ...this.uploadedFiles,
+                {
+                  ...f,
+                  parentType: "company",
+                  parentId: 1,
+                  isDeleted: false,
+                  createdDate: DateTime.now(),
+                },
+              ];
+            });
+            this.sweetAlertService.success(response.message);
+          }
+        },
+        error: (error) => {
+          console.error("Error uploading documents:", error);
+        },
+      });
+    } else {
+      console.log("No files selected.");
+    }
+  }
+
+  uploadFiles(fileType: string): void {
+    switch (fileType) {
+      case "video":
+        this.uploadDocument(this.selectedVideos);
+        break;
+      case "ppt":
+        this.uploadDocument(this.selectedPpts);
+        break;
+      case "audio":
+        this.uploadDocument(this.selectedAudios);
+        break;
+      case "document":
+        this.uploadDocument(this.selectedDocuments);
+        break;
+      default:
+        break;
+    }
+  }
+
+  saveUploadedFilesData(): void {
+    this.uploadedFiles?.flatMap((doc) => {
+      try {
+        this.editProfileApiService.saveDocumentDetails(doc).subscribe({
+          next: (response: { success: boolean; message: any }) => {
+            if (response.success) {
+              console.log(response.success, 'success');
+            } else {
+            }
+          },
+          error: (error) => {},
+        });
+      } catch (e) {
+        console.log(e);
+      }
+    });
+  }
+
   async onSubmit() {
     // const CompanyProfile: Partial<PostCompanydatum> = this.formData.value;
     //     const confirmed = await this.sweetAlertService.confirm(
@@ -272,6 +381,7 @@ export class EditProfileComponent implements OnInit {
             console.log(response);
             if (response.success) {
               this.sweetAlertService.success(response.message);
+              this.saveUploadedFilesData();
             } else {
               this.sweetAlertService.error(response.message);
             }
