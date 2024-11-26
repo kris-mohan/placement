debcebf4f12a7cdc58b7052cf32d08ca173f6f90
Usha <ushanuchin99@gmail.com>
2024-11-26 09:44:00 +0530

    student result infrmation

--- web-app/src/app/features/company-menu/test-interviews/student-result-information/StudentResultInformationApiService.ts
+++ web-app/src/app/features/company-menu/test-interviews/student-result-information/StudentResultInformationApiService.ts
@@ -1,13 +1,13 @@
-import { HttpErrorResponse } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { catchError, firstValueFrom, Observable, throwError } from 'rxjs';
-import { ApiHttpService } from 'src/app/services/api-services/api-http-services';
-import { Jobposting } from 'src/app/services/types/Jobposting';
-import { Jobinterviewround } from 'src/app/services/types/Jobinterviewround';
-import { ODataEntity } from 'src/app/services/types/OData';
+import { HttpErrorResponse } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { catchError, firstValueFrom, Observable, throwError } from "rxjs";
+import { ApiHttpService } from "src/app/services/api-services/api-http-services";
+import { Jobposting } from "src/app/services/types/Jobposting";
+import { Jobinterviewround } from "src/app/services/types/Jobinterviewround";
+import { ODataEntity } from "src/app/services/types/OData";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class StudentResultInformationApiService {
   constructor(private apiHttpService: ApiHttpService) {}
@@ -38,9 +38,17 @@ export class StudentResultInformationApiService {
     id: number | null,
     IndentForm: Jobposting
   ): Observable<ODataEntity<Jobposting[]>> {
-    const url = `/JobpostStudentround${id ? `?key=${id}` : ''}`;
+    const url = `/JobpostStudentround${id ? `?key=${id}` : ""}`;
     return id
       ? this.apiHttpService.patch(url, IndentForm)
       : this.apiHttpService.post(url, IndentForm);
+  }
+
+  public GetJobInterviewRounds(
+    id: number
+  ): Observable<ODataEntity<Jobposting[]>> {
+    return this.apiHttpService.get<ODataEntity<Jobposting[]>>(
+      `/Jobposting?$expand=Jobinterviewrounds($expand=JobpostStudentrounds($expand=Student($expand=Org,Batch,Studentacademics($expand=Course))))&$filter=Id eq ${id}`
+    );
   }
 }
